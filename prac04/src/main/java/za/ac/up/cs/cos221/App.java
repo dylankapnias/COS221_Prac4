0b77ffac521cda8ee4d9ccd03f996946062accdc
Dylan Kapnias <72029240+dylankapnias@users.noreply.github.com>
2022-05-03 19:22:14 +0200

    Base project
    Added relevant developer information to the pom.xml
    
    Initial connection to the database using environment variables.

--- prac04/src/main/java/za/ac/up/cs/cos221/App.java
+++ prac04/src/main/java/za/ac/up/cs/cos221/App.java
@@ -1,13 +1,61 @@
 package za.ac.up.cs.cos221;
 
+import javax.swing.*;
+import java.sql.*;
+import java.io.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static JFrame createGUI() {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setLayout(null);
+        mainFrame.setVisible(true);
+        mainFrame.pack();
+        mainFrame.setTitle("COS221_Prac4");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        return mainFrame;
+    }
+
+    public static Connection createConnection() {
+        String connString = new StringBuilder()
+        .append("jdbc:")
+        .append(System.getenv("SAKILA_DB_PROTO") + "://")
+        .append(System.getenv("SAKILA_DB_HOST") + ":")
+        .append(System.getenv("SAKILA_DB_PORT") + "/")
+        .append(System.getenv("SAKILA_DB_NAME") + "?")
+        .append("user=" + System.getenv("SAKILA_DB_USERNAME") + "&")
+        .append("password=" + System.getenv("SAKILA_DB_PASSWORD")).toString();
+
+        try {
+            Connection conn = DriverManager.getConnection(connString);
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //JFrame gui = createGUI();
+
+        Connection conn = createConnection();
+        if (conn == null) {
+            System.out.println("Failed to connect to database!");
+            System.exit(1);
+        }
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM store");
+            while(rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
