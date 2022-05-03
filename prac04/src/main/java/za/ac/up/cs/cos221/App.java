673757a2a0871b3e6b37bc07be11f578d3f078fd
Dylan Kapnias <72029240+dylankapnias@users.noreply.github.com>
2022-05-03 23:13:47 +0200

    Update App.java
    Staff and Film table complete

--- prac04/src/main/java/za/ac/up/cs/cos221/App.java
+++ prac04/src/main/java/za/ac/up/cs/cos221/App.java
@@ -1,20 +1,250 @@
 package za.ac.up.cs.cos221;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 import java.io.*;
+import java.util.Vector;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static JPanel createStaffTab() {
+        JPanel control = new JPanel();
+        control.setLayout(new BorderLayout());
+
+        JPanel filter = new JPanel();
+        filter.setLayout(new BorderLayout());
+
+        JLabel fl = new JLabel("Filter: ");
+        filter.add(fl, BorderLayout.WEST);
+
+        JTextField tf = new JTextField(45);
+        filter.add(tf, BorderLayout.CENTER);
+
+
+        control.add(filter, BorderLayout.NORTH);
+
+        Vector<String> c = new Vector<>();
+        Vector<Vector<String>> d = new Vector<>();
+        Vector<String> data = new Vector<>();
+
+        c.add("First Name");
+        c.add("Last Name");
+        c.add("Address");
+        c.add("Address 2");
+        c.add("District");
+        c.add("City");
+        c.add("Postal Code");
+        c.add("Phone");
+        c.add("Store");
+        c.add("Active");
+
+        Connection conn = createConnection();
+        if (conn == null) {
+            System.out.println("Failed to connect to database!");
+            System.exit(1);
+        }
+
+        String q = "SELECT s.first_name, s.last_name, a.address, a.address2, a.district, c.city, a.postal_code, a.phone, s.store_id, s.active " +
+                   "FROM staff AS s JOIN address AS a " +
+                   "ON s.address_id = a.address_id JOIN city AS c ON a.city_id = c.city_id";
+
+        ResultSet res = queryResults(conn, q);
+        try {
+            while (res.next()) {
+                data.add(res.getString("first_name"));
+                data.add(res.getString("last_name"));
+                data.add(res.getString("address"));
+                data.add(res.getString("address2"));
+                data.add(res.getString("district"));
+                data.add(res.getString("city"));
+                data.add(res.getString("postal_code"));
+                data.add(res.getString("phone"));
+                data.add(res.getString("store_id"));
+                data.add(res.getString("active"));
+
+                d.add(new Vector<String>(data));
+                data.clear();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        DefaultTableModel m = new DefaultTableModel(d, c);
+
+        JTable t = new JTable(m);
+        t.setShowGrid(true);
+        t.setShowVerticalLines(true);
+
+        JScrollPane s = new JScrollPane(t);
+        s.setBounds(10, 30, 40, 40);
+
+        TableRowSorter<TableModel> sort = new TableRowSorter<>(t.getModel());
+        t.setRowSorter(sort);
+        tf.getDocument().addDocumentListener(new DocumentListener()
+        {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String str = tf.getText();
+                if (str.trim().length() == 0) {
+                    sort.setRowFilter(null);
+                } else {
+                    sort.setRowFilter(RowFilter.regexFilter("(?i)" + str));
+                }
+            }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String str = tf.getText();
+                if (str.trim().length() == 0) {
+                    sort.setRowFilter(null);
+                } else {
+                    sort.setRowFilter(RowFilter.regexFilter("(?i)" + str));
+                }
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e) {}
+        });
+
+        control.add(s);
+
+        return control;
+    }
+
+    public static JScrollPane createFilmTable() {
+        Vector<String> c = new Vector<>();
+        Vector<Vector<String>> d = new Vector<>();
+        Vector<String> data = new Vector<>();
+
+        c.add("Film ID");
+        c.add("Title");
+        c.add("Description");
+        c.add("Year of Release");
+        c.add("Language");
+        c.add("Original Language");
+        c.add("Rental Duration");
+        c.add("Rental Price");
+        c.add("Length");
+        c.add("Cost of Replacement");
+        c.add("Rating");
+        c.add("Special Features");
+        c.add("Last Updated");
+
+        Connection conn = createConnection();
+        if (conn == null) {
+            System.out.println("Failed to connect to database!");
+            System.exit(1);
+        }
+
+        String q = "SELECT * " +
+                "FROM film ";
+
+        ResultSet res = queryResults(conn, q);
+        try {
+            while (res.next()) {
+                data.add(res.getString("film_id"));
+                data.add(res.getString("title"));
+                data.add(res.getString("description"));
+                data.add(res.getString("release_year"));
+                data.add(res.getString("language_id"));
+                data.add(res.getString("original_language_id"));
+                data.add(res.getString("rental_duration"));
+                data.add(res.getString("rental_rate"));
+                data.add(res.getString("length"));
+                data.add(res.getString("replacement_cost"));
+                data.add(res.getString("rating"));
+                data.add(res.getString("special_features"));
+                data.add(res.getString("last_update"));
+
+                d.add(new Vector<String>(data));
+                data.clear();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        DefaultTableModel m = new DefaultTableModel(d, c);
+
+        JTable t = new JTable(m);
+        t.setShowGrid(true);
+        t.setShowVerticalLines(true);
+
+        TableColumnModel columnModel = t.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(1);
+        columnModel.getColumn(3).setPreferredWidth(1);
+        columnModel.getColumn(4).setPreferredWidth(1);
+        columnModel.getColumn(5).setPreferredWidth(1);
+        columnModel.getColumn(6).setPreferredWidth(1);
+        columnModel.getColumn(7).setPreferredWidth(1);
+        columnModel.getColumn(8).setPreferredWidth(1);
+        columnModel.getColumn(9).setPreferredWidth(1);
+        columnModel.getColumn(10).setPreferredWidth(1);
+
+        JScrollPane s = new JScrollPane(t);
+        s.setBounds(10, 30, 40, 40);
+
+        return s;
+    }
+
+    public static JPanel createFilmsTab() {
+        JPanel control = new JPanel();
+        control.setLayout(new BorderLayout());
+
+        JPanel bSpace = new JPanel();
+        bSpace.setLayout(new BorderLayout());
+
+        JButton b = new JButton("Add Film");
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(e);
+            }
+        });
+        bSpace.add(b, BorderLayout.CENTER);
+
+        control.add(bSpace, BorderLayout.NORTH);
+
+        control.add(createFilmTable());
+
+        return control;
+    }
+
+    public static JPanel createInventoryTab() { return null; }
+
+    public static JPanel createClientsTab() { return null; }
     public static JFrame createGUI() {
         JFrame mainFrame = new JFrame();
-        mainFrame.setLayout(null);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        mainPanel.setLayout(new GridLayout(1, 1));
+
+        JTabbedPane tabs = new JTabbedPane();
+
+        tabs.addTab("Staff", createStaffTab());
+        tabs.addTab("Films", createFilmsTab());
+        tabs.addTab("Inventory", new JLabel("Inventory"));
+        tabs.addTab("Clients", new JLabel("Clients"));
+
+        mainPanel.add(tabs);
+
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
         mainFrame.setVisible(true);
-        mainFrame.pack();
+        //mainFrame.pack();
+        mainFrame.setSize(2000, 600);
         mainFrame.setTitle("COS221_Prac4");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -29,11 +259,21 @@ public class App
         .append(System.getenv("SAKILA_DB_PORT") + "/")
         .append(System.getenv("SAKILA_DB_NAME") + "?")
         .append("user=" + System.getenv("SAKILA_DB_USERNAME") + "&")
-        .append("password=" + System.getenv("SAKILA_DB_PASSWORD")).toString();
+        .append("password=" + System.getenv("SAKILA_DB_PASSWORD"))
+        .toString();
 
         try {
-            Connection conn = DriverManager.getConnection(connString);
-            return conn;
+            return DriverManager.getConnection(connString);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ResultSet queryResults(Connection c, String q) {
+        try {
+            Statement stmt = c.createStatement();
+            return stmt.executeQuery(q);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
@@ -41,21 +281,6 @@ public class App
     }
     public static void main( String[] args )
     {
-        //JFrame gui = createGUI();
-
-        Connection conn = createConnection();
-        if (conn == null) {
-            System.out.println("Failed to connect to database!");
-            System.exit(1);
-        }
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM store");
-            while(rs.next()) {
-                System.out.println(rs.getString(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        JFrame gui = createGUI();
     }
 }
